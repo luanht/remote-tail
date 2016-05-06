f28a72e3add1b39ff3bbe5f3737de9856a4973a2
luanht <htluan2605@gmail.com>
2016-05-06 11:06:13 +0700

    intial commit

---
+++ node-server/app.js
@@ -0,0 +1,80 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var auth = require('http-auth');
+
+var ROOT_DIR = path.join(__dirname + '/../');
+var config = JSON.parse(fs.readFileSync(ROOT_DIR + '/config.json'));
+var basic = auth.basic({
+    realm: "Simon Area."
+  }, function (username, password, callback) {
+    callback(username === config.auth.username && password === config.auth.password);
+  }
+);
+
+http.createServer(basic, function (req, res) {
+   var requestUrl = url.parse(req.url, true);
+   switch(requestUrl.pathname) {
+    // Index
+    case '/':
+      fs.readFile('index.html', "binary", function(err, file) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(fs.readFileSync(ROOT_DIR + '/index.html'));
+        res.end();
+      });
+      break;
+    // Watching
+    case '/watch':
+      res.writeHead(200, {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive"
+      });
+      res.write("retry: 10000\n\n");
+
+      try {
+        var watchFile = requestUrl.query.filePath;
+
+        fs.stat(watchFile, function(err, stat) {
+          if(err == null) {
+            Tail = require('tail').Tail;
+
+            var tail = new Tail(watchFile);
+            tail.on("line", function(data) {
+              res.write("data: " + data + "\n\n");
+            });
+
+            tail.on("error", function(err) {
+              res.write('event: exception\n');
+              res.write('data: {"message": " ' + err +'"}\n\n');
+            });
+
+            res.on('end', function () {
+              tail.unwatch();
+            });
+
+          } else if (err.code == 'ENOENT') {
+            res.write('event: exception\n');
+            res.write('data: {"message": "File is not exists!"}\n\n');
+            res.end();
+          } else {
+            res.write('event: exception\n');
+            res.write('data: {"message": "' + err +'"}\n\n');
+            res.end();
+          }
+        });
+
+      } catch(err) {
+        res.write('event: exception\n');
+        res.write('data: {"message": "' + err +'"}\n\n');
+      }
+      break;
+    default:
+      res.writeHead(404);
+      res.write("<h1>Not found</h1>");
+      res.end();
+   }
+}).listen(config.server.port);
+
+
---
+++ ruby-server/server.rb
@@ -0,0 +1,54 @@
+WORKING_DIR = File.expand_path File.dirname(__FILE__)
+ROOT_DIR    = File.expand_path("..", WORKING_DIR)
+
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('Gemfile', WORKING_DIR)
+require 'rubygems'
+require 'bundler/setup'
+require 'json'
+require 'sinatra'
+require 'sinatra/basic_auth'
+Bundler.require
+
+config = JSON.parse File.read(File.join(ROOT_DIR, 'config.json'))
+
+authorize do |username, password|
+  username == config["auth"]["username"] && password == config["auth"]["password"]
+end
+
+configure do
+  set :port, config["server"]["port"]
+end
+
+protect do
+  # Index
+  get '/' do
+    IO.read(ROOT_DIR + '/index.html')
+  end
+
+  # Watching
+  get '/watch' do
+    headers \
+      "Content-Type"  => "text/event-stream",
+      "Cache-Control" => "no-cache",
+      "Connection"    => "keep-alive"
+
+    watchFile = request["filePath"]
+    stream do |out|
+      out << "retry: 10000\n\n"
+
+      if File::readable?(watchFile)
+        File.open(watchFile) do |file|
+          file.extend(File::Tail)
+          file.backward(0)
+          file.interval = 0.01
+          file.tail do |line|
+            out << "data: " << line << "\n\n"
+          end
+        end
+      else
+        out << "event: exception\n"
+        out << "data: {\"message\": \"File is not exists or unreadable\"}\n\n"
+      end
+    end
+  end
+end
